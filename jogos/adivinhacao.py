a3631e54b1d8566eb3ddadc20f492664a12bf212
schatderer <schatderer@posteo.ie>
2022-07-22 22:04:09 -0300

    Python project

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,13 +1,13 @@
 import random
 
+
 def jogar():
 
-    print("*********************************")
+    print("#################################")
     print("Bem vindo ao jogo de Adivinhação!")
-    print("*********************************")
+    print("#################################")
 
-    numero_secreto = random.randrange(1,101)
-    total_de_tentativas = 0
+    numero_secreto = random.randrange(1, 101)
     pontos = 1000
 
     print("Qual nível de dificuldade?")
@@ -15,9 +15,9 @@ def jogar():
 
     nivel = int(input("Defina o nível: "))
 
-    if(nivel == 1):
+    if nivel == 1:
         total_de_tentativas = 20
-    elif(nivel == 2):
+    elif nivel == 2:
         total_de_tentativas = 10
     else:
         total_de_tentativas = 5
@@ -26,29 +26,30 @@ def jogar():
         print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
         chute_str = input("Digite um número entre 1 e 100: ")
-        print("Você digitou " , chute_str)
+        print("Você digitou ", chute_str)
         chute = int(chute_str)
 
-        if(chute < 1 or chute > 100):
+        if chute < 1 or chute > 100:
             print("Você deve digitar um número entre 1 e 100!")
             continue
 
         acertou = chute == numero_secreto
-        maior   = chute > numero_secreto
-        menor   = chute < numero_secreto
+        maior = chute > numero_secreto
+        menor = chute < numero_secreto
 
-        if(acertou):
+        if acertou:
             print("Você acertou e fez {} pontos!".format(pontos))
             break
         else:
-            if(maior):
+            if maior:
                 print("Você errou! O seu chute foi maior do que o número secreto.")
-            elif(menor):
+            elif menor:
                 print("Você errou! O seu chute foi menor do que o número secreto.")
             pontos_perdidos = abs(numero_secreto - chute)
             pontos = pontos - pontos_perdidos
 
     print("Fim do jogo")
 
-if(__name__ == "__main__"):
+
+if __name__ == "__main__":
     jogar()
